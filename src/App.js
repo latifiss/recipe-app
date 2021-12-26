10c4cb879d00134eabf45cf503608c1aecd614cf
latif <latifissaka069@gmail.com>
2021-12-26 09:35:55 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import RecipeCard from './components/RecipeCard';
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  //function to search for the recipes
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = apiUrl + query;
+    const res = await fetch(url);
+    const data = await res.json()
+    console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+      searchRecipes()
+  }, []);
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    searchRecipes()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Our Recipe App</h1>
+      <SearchBar
+        handleSubmit={handleSubmit}
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+        isLoading={isLoading}
+      />
+      <div className='recipes'>
+        {recipes ? recipes.map(recipe => (
+          <RecipeCard
+          key={recipe.idMeal}
+          recipe={recipe}
+          />
+        )): 'No recipes!'
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SearchBar =({
+    value, isLoading, handleSubmit, onChange
+}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+            value={value}
+            disabled= {isLoading}
+            onChange={onChange}            
+            placeholder='Search Recipes'
+            className='form-control'
+            />
+            <input 
+            disabled={isLoading || !value }
+            type='submit'
+            className='btn'
+            value='search'
+            />
+        </form>
+    )
+}
+
+
+export default SearchBar;
